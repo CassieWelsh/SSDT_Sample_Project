960393ecdb699dd42631f519d4a9cd26418a6a66
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2022-07-22 15:04:55 +0300

    Create "Product" and "Order" tables

---
+++ dbo/Tables/Order.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Product_ID] INT NOT NULL, 
+    [Person_ID] INT NOT NULL, 
+    CONSTRAINT [FK_Product_ToOrder] FOREIGN KEY ([Product_ID]) REFERENCES [Product]([Id]),
+    CONSTRAINT [FK_Person_ToOrder] FOREIGN KEY ([Person_ID]) REFERENCES [Person]([Id]) 
+
+)
---
+++ dbo/Tables/Product.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(40) NULL, 
+    [Cost] NCHAR(10) NULL, 
+    [Weight] FLOAT NULL
+)
