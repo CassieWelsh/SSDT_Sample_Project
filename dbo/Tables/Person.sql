25b9c1cb61882386d24e2c321ce785953de99a51
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2022-07-22 14:47:49 +0300

    Import "Sampledb" database

---
+++ dbo/Tables/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [Id]       INT        NOT NULL,
+    [Name]     NCHAR (30) NOT NULL,
+    [Age]      INT        NULL,
+    [Hometown] NCHAR (50) NULL,
+    [EyeColor] NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
